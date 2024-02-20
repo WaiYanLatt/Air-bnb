d2a03b817899abc7096df172b027f6565114fa11
Wai Yan Latt <humanant67@gmail.com>
2024-02-20 15:16:49 +0630

    na

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -90,7 +90,7 @@ export default {
       class="bg-gray-100 py-5 lg:px-28 px-5 border-b-2 border-gray-200"
       v-show="isNavshow === true"
     >
-      <h1 class="font-bold underline text-md text-center cursor-pointer">
+      <h1 class="font-bold underline lg:text-md text-sm text-center cursor-pointer">
         Learn about Guest Favorites, the most loved homes on AirBnb
       </h1>
     </div>
@@ -194,7 +194,7 @@ export default {
     <!-- Model Box -->
     <div
       v-show="show === true"
-      class="bg-white object-cover bg-no-repeat animate__backInUp animate__animated rounded-lg border shadow-xl h-auto w-[370px] lg:w-[700px] absolute z-10 top-36 lg:top-28 left-3 lg:left-[22%] p-5 lg:p-10"
+      class="bg-white object-cover bg-no-repeat animate__backInUp animate__animated rounded-lg border shadow-xl h-auto w-full lg:w-[700px] absolute z-10 top-36 lg:top-28 lg:left-[22%] p-5 lg:p-10"
     >
       <h1 class="text-right text-red-500 cursor-pointer" @click="unShow">
         <i class="fa-solid fa-xmark mb-5 text-xl"></i>
@@ -203,7 +203,7 @@ export default {
     </div>
     <div
       v-show="showForm === true"
-      class="bg-white object-cover bg-no-repeat animate__backInUp animate__animated rounded-lg border shadow-xl h-auto w-[370px] lg:w-[700px] absolute z-10 top-36 lg:top-28 left-3 lg:left-[22%] p-5 lg:p-10"
+      class="bg-white object-cover bg-no-repeat animate__backInUp animate__animated rounded-lg border shadow-xl h-auto w-full lg:w-[700px] absolute z-10 top-36 lg:top-28 lg:left-[22%] p-5 lg:p-10"
     >
       <h1 class="text-right text-red-500 cursor-pointer" @click="unShow">
         <i class="fa-solid fa-xmark mb-5 text-xl"></i>
