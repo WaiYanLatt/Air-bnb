c264395f3c702321523e5683e147f8fbbd1efaf7
Wai Yan Latt <108973955+WaiYanLatt@users.noreply.github.com>
2024-02-24 16:11:01 +0630

    Update stay.js

--- src/pinia/stay.js
+++ src/pinia/stay.js
@@ -19,17 +19,17 @@ const useStay = defineStore("stay", {
         options
       );
       const data = await response.json();
-      let indicesToRemove = [
-        1, 5, 6, 13, 17, 21, 23, 24, 28, 30, 34, 35, 37, 39, 40, 42, 45, 46, 48,
-        56, 59, 60, 64, 66, 71, 72, 75, 76, 78, 80, 82, 84, 85, 87, 91, 93, 95,
-        99,
-      ]; // indices of the elements you want to remove
+      // let indicesToRemove = [
+      //   1, 5, 6, 13, 17, 21, 23, 24, 28, 30, 34, 35, 37, 39, 40, 42, 45, 46, 48,
+      //   56, 59, 60, 64, 66, 71, 72, 75, 76, 78, 80, 82, 84, 85, 87, 91, 93, 95,
+      //   99,
+      // ]; // indices of the elements you want to remove
 
-      // Remove elements at the specified indices
-      indicesToRemove.sort((a, b) => b - a); // Sorting in descending order to avoid index issues
-      indicesToRemove.forEach((index) => {
-        data.results.splice(index, 1);
-      });
+      // // Remove elements at the specified indices
+      // indicesToRemove.sort((a, b) => b - a); // Sorting in descending order to avoid index issues
+      // indicesToRemove.forEach((index) => {
+      //   data.results.splice(index, 1);
+      // });
       return data.results;
     },
 
